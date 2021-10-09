81e3dd4d0f0f8a1b906c4c0ba4373ce334133089
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2021-10-09 21:16:07 +0700

    Solving Problem 4

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    public float moveSpeed = 100f;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            rigidBody2D.velocity = new Vector2(0, moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rigidBody2D.velocity = new Vector2(-moveSpeed * Time.deltaTime, 0);
+        }        
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            rigidBody2D.velocity = new Vector2(0, -moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rigidBody2D.velocity = new Vector2(moveSpeed * Time.deltaTime, 0);
+        }
+    }
+}
