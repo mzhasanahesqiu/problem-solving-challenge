0394b1174c50b001c5364da504ebabe23751d240
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2021-10-10 15:06:21 +0700

    Solving Problem 5

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    Vector3 mousePosition;
+    public float moveSpeed = 0.01f;
+    private Rigidbody2D rigidBody2D;
+    Vector2 position = new Vector2(0f, 0f);
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody2D.MovePosition(position);
+    }
+}
