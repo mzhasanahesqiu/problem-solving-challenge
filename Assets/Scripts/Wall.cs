a93a3996df271ba0f61b89eb8e8503bcec3bddfa
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2021-10-10 23:25:20 +0700

    Solving Problem 8

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+}
