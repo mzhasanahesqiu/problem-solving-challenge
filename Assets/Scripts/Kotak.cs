58a0d850c92eb5465a61ad031d52b48b1c9dcf1f
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2021-10-11 01:12:45 +0700

    Solving Problem 9

---
+++ Assets/Scripts/Kotak.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kotak : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    public int scoreValue = 1;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();        
+    }    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Score.score += scoreValue;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public GameObject[] kotak;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+    private Rigidbody2D rb;
+    private GameObject ball;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+        int randKotak = Random.Range(0, kotak.Length);
+
+        Instantiate(kotak[randKotak], spawnPoints[randSpawnPoint].position, transform.rotation);
+    }
+}
