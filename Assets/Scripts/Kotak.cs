b84f86b37f50b01304f6fa416d43606482659e9b
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2021-10-10 16:44:48 +0700

    Problem Solving 7

---
+++ Assets/Scripts/Kotak.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Kotak : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    public static int score;
+    public int scoreValue = 1;
+    public Text _score;
+    Text text;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        text = GetComponent<Text>();
+        score = 0;
+    }    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+            score += scoreValue;
+            Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        _score.text = "Skor: " + score;
+    }
+}
